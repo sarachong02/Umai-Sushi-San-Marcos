7431711822ac7800abcbec6af27536bca8381fcb
schong6 <schong6@uci.edu>
2024-09-25 14:28:16 -0700

    edited owners info

--- src/pages/Home/AboutChef/AboutChef.js
+++ src/pages/Home/AboutChef/AboutChef.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './AboutChef.css'
 
+import bgImage from "../../../assets/owners.png"
+
 const AboutChef = () => {
   return (
     <div>
@@ -24,34 +26,26 @@ const AboutChef = () => {
             <div className='grid-container py-md'>
                 <div className='grid-item chef-info'>
                     <h3 className='heading-tertiary'>
-                        Chris <span className='bold-emphasis'>Chong</span>
+                        <span className='bold-emphasis'>Chris</span> and <span className='bold-emphasis'>Ana</span>
                     </h3>
 
-                    <h4>Executive Chef and Owner</h4>
+                    <h4>the owners</h4>
 
-                    <p>With humble roots in learning about sushi in Chicago, Chris has spent decades 
-                        in San Diego perfecting this art
+                    <p> Chris, the executive chef, studied and worked at various sushi restaurants in Chicago and New York,
+                        learning the skills of a master sushi chef, while also studying the restaurant business. After marrying
+                        Ana, the two proceeded to start their own sushi restaurant from humble beginnings, first in Convoy,
+                        then a buffet in Escondido, and now <span className='bold-emphasis'>Umai Sushi </span>in San Marcos. 
+                    </p>
+
+                    <p>The couple has been in the restaurant business for over 20 years, and there have been many ups and downs
+                        including the 2008 Repression and even most recently, COVID-19. However, they have proceeded to push 
+                        through — and continue today — to accomplish their goal to consistently serve the local community 
+                        <span className='bold-emphasis'> high quality</span> Japanese cuisine.
                     </p>
                 </div>
 
-                <div className='grid-item chef-info'>
-                    <h3 className='heading-tertiary'>
-                        Enoch <span className='bold-emphasis'>Lorem</span>
-                    </h3>
-
-                    <h4>Sous Chef</h4>
-
-                    <p> 20+ years of crafting the finest sushi</p>
-                </div>
-
-                <div className='grid-item chef-info'>
-                    <h3 className='heading-tertiary'>
-                        Ana <span className='bold-emphasis'>Han</span>
-                    </h3>
-
-                    <h4>Head Manager</h4>
-
-                    <p>Delivering sushi to your table with the finest service</p>
+                <div>
+                    <img src={bgImage} alt='owners' className='owners-img'/>
                 </div>
             </div>
         </div>
